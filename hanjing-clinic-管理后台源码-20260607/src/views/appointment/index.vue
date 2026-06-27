40fe5efd5843dfaab0809f88bc9b0e77b630c214
enwey <enwey1992@gmail.com>
2026-06-27 22:37:12 +0800

    style: 调整列表默认列宽以确保默认初始状态下完整显示单行内容

--- hanjing-clinic-管理后台源码-20260607/src/views/appointment/index.vue
+++ hanjing-clinic-管理后台源码-20260607/src/views/appointment/index.vue
@@ -652,14 +652,14 @@ async function submitCheckout() {
         <table class="data-table" v-resizable>
           <thead>
             <tr>
-              <th style="width: 140px;">预约单号</th>
-              <th style="width: 180px;">患者</th>
-              <th style="width: 120px;">门店</th>
+              <th style="width: 160px;">预约单号</th>
+              <th style="width: 200px;">患者</th>
+              <th style="width: 160px;">门店</th>
               <th style="width: 100px;">医生</th>
-              <th style="width: 140px;">预约时间</th>
-              <th style="width: 90px;">来源</th>
+              <th style="width: 160px;">预约时间</th>
+              <th style="width: 100px;">来源</th>
               <th style="width: 100px;">状态</th>
-              <th style="width: 170px;">预约创建时间</th>
+              <th style="width: 200px;">预约创建时间</th>
               <th :style="{ width: operationColumnWidth, minWidth: operationColumnWidth, textAlign: 'right' }">操作</th>
             </tr>
           </thead>
